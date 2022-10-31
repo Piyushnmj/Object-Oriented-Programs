f038f673200dbc2e670b10a9c3575e6f37f25177
Piyushnmj <piyushnmj@yahoo.com>
2022-10-31 11:26:16 +0530

    Inventory Data Management using JSON File

--- ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
+++ ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace ObjectOrientedPrograms
+﻿using ObjectOrientedPrograms.Repository;
+
+namespace ObjectOrientedPrograms
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Object Oriented Programs");
+            Console.WriteLine("Welcome to Object Oriented Program\n");
+
+            InventoryManager inventoryManager = new InventoryManager();
+            inventoryManager.CalcInventoryValue();
         }
     }
 }
