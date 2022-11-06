0c397988fc77be36136d972508561dc5d7154eaa
Piyushnmj <piyushnmj@yahoo.com>
2022-11-06 17:51:07 +0530

    Keeping a track of Stock Account of a Customer

--- ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
+++ ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
@@ -6,10 +6,39 @@ namespace ObjectOrientedPrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Object Oriented Program\n");
+            Console.WriteLine("Welcome to Object Oriented Program");
 
-            StockManager stockManager = new StockManager();
-            stockManager.CalcStockValue();
+            Console.WriteLine("\nPress 1 to Enter");
+            int run = Convert.ToInt32(Console.ReadLine());
+            while (run == 1)
+            {
+                CommercialDataProcessing objDataProcessing = new CommercialDataProcessing();
+                Console.WriteLine("\nSelect an Option from below: \n1.Buy \n2.Sell \n3.Total Value of Account");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        {
+                            objDataProcessing.CompanyStockList();
+                            objDataProcessing.Buy();
+                            break;
+                        }
+                    case 2:
+                        {
+                            objDataProcessing.CustomerStockList();
+                            objDataProcessing.Sell();
+                            break;
+                        }
+                    case 3:
+                        {
+                            objDataProcessing.valueOf();
+                            break;
+                        }
+                }
+                Console.WriteLine("\nPress 1 to Enter \nPress 2 to Exit");
+                run = Convert.ToInt32(Console.ReadLine());
+            }
         }
     }
 }
